4e71f538c41d4603f52fdf7ba7de09717d5a6329
Mohsen Kokabi <mkokabi@yahoo.com>
2019-12-08 06:58:29 +1100

    Adding unit tests.

---
+++ TestProject/RemarkServiceTests.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class RemarkServiceTests
+    {
+        [Fact]
+        public void GetUserNotesWithLocation()
+        {
+
+        }
+
+        [Fact]
+        public void StoreNoteOnLocation()
+        {
+        }
+
+        [Fact]
+        public void GetNotesTakenOnLocation()
+        {
+
+        }
+
+        [Fact]
+        public void SearchNotesByUsername()
+        {
+
+        }
+
+        [Fact]
+        public void SearchNotesByText()
+        {
+
+        }
+    }
+}
---
+++ TestProject/UserServiceTests.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public void RegisterUser_NewUserRecordShouldBeCreated()
+        {
+
+        }
+
+        [Fact]
+        public void RegisterUser_DuplicateUsernameOrEmail_ShouldThrow()
+        {
+        }
+
+        [Fact]
+        public void Login_ShouldAllowWihtUserNameOrEmail()
+        {
+
+        }
+    }
+}
